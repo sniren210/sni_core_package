a11c210b82d942d8432d53db13ba091c7439c193
sniren210 <sniren2002@gmail.com>
2023-01-17 17:01:13 +0700

    fix: linter

--- lib/src/core/helpers/sni.dart
+++ lib/src/core/helpers/sni.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_field, unused_element
+
 part of sni_core;
 
 class Sni {
--- lib/src/core/injector/settings/settings.dart
+++ lib/src/core/injector/settings/settings.dart
@@ -1,7 +1,6 @@
 part of sni_core;
 
 Future<void> _initializeSettings({
-  String? defaultCurrencyCode,
   String? defaultCountryCode,
   String? defaultLanguageCode,
 }) async {
